085e15bbc2ff9319faef45dafe96abea7d4c50d4
Yauheni Belash <something@site.com>
2018-02-24 20:48:33 +0300

    Create Sorter class with all functions

--- src/index.js
+++ src/index.js
@@ -1,30 +1,39 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.arr = [];
+    this.compareFunction = (a, b) => a - b;
   }
 
   add(element) {
-    // your implementation
+    this.arr.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.arr[index];
   }
 
   get length() {
-    // your implementation
+    return this.arr.length;
   }
 
   toArray() {
-    // your implementation
+    return this.arr;
   }
 
   sort(indices) {
-    // your implementation
+    indices.sort((a, b) => a - b);
+    let newArr = [];
+    indices.forEach(element => {
+      newArr.push(this.arr[element]);
+    });
+    newArr.sort(this.compareFunction);
+    indices.forEach(element => {
+      this.arr[element] = newArr.shift();
+    });
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
